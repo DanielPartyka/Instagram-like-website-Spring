304714e98c683951776e3ba68520728abc57a62c
Galan <partykadaniel@poczta.fm>
2020-04-13 18:06:48 +0200

    Initial commit

---
+++ src/main/java/com/example/portalspolecznosciowy/PortalspolecznosciowyApplication.java
@@ -0,0 +1,13 @@
+package com.example.portalspolecznosciowy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalspolecznosciowyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalspolecznosciowyApplication.class, args);
+	}
+
+}
