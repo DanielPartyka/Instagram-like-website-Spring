eab98f142ae6eb4e3e9f017dfdc063ba95498201
Galan <partykadaniel@poczta.fm>
2020-04-24 16:56:05 +0200

    Todo Add Following sytem in index.html

--- src/main/java/com/example/portalspolecznosciowy/controllers/portalspolecznosciowyController.java
+++ src/main/java/com/example/portalspolecznosciowy/controllers/portalspolecznosciowyController.java
@@ -1,11 +1,18 @@
 package com.example.portalspolecznosciowy.controllers;
 
+import com.example.portalspolecznosciowy.models.Followers;
+import com.example.portalspolecznosciowy.models.User;
 import com.example.portalspolecznosciowy.repositories.UserRepository;
 import com.example.portalspolecznosciowy.services.SectionsServices;
 import com.example.portalspolecznosciowy.services.UserServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -14,11 +21,25 @@ public class portalspolecznosciowyController {
     @Autowired
     private UserServices userServices;
 
+    public static String user;
+
     @GetMapping("/")
     public ModelAndView indexrender()
     {
         ModelAndView modelAndView = new ModelAndView("index");
         modelAndView.addObject("users",userServices.proponowaniUzytkownicy());
+        return modelAndView;
+    }
+    @PostMapping("/")
+    public ModelAndView indexfollow(@ModelAttribute("followers") Followers followers, BindingResult result) {
+        if (result.hasErrors()) {
+            System.out.println("Jest problem z " + result);
+        }
+        ModelAndView modelAndView = new ModelAndView("index");
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        user = authentication.getName();
+        //setFollower.userid//userServices.findUserIdLong(user);
+
         return modelAndView;
     }
 
